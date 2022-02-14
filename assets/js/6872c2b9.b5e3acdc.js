8dd3e76d3b38741f54be2cf568679cb0bc231b73
Gahl Saraf <saraf.gahl@gmail.com>
2022-02-14 12:29:58 +0200

    update

--- assets/js/6872c2b9.b5e3acdc.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[214],{9157:function(e){e.exports=JSON.parse('{"title":"Configuration","slug":"config","permalink":"/docs/config","navigation":{"previous":{"title":"JetBrains","permalink":"/docs/debugging/JetBrains"},"next":{"title":"raftt.yml","permalink":"/docs/config/raftt.yml"}}}')}}]);
