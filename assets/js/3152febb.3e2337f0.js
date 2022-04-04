26e3e1f6381582e435a93f5927b15ebb856f9b79
Gahl Saraf <saraf.gahl@gmail.com>
2022-04-04 14:06:57 +0300

    update docs

--- assets/js/3152febb.3e2337f0.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[225],{3492:function(t){t.exports=JSON.parse('{"title":"Getting Started","slug":"basics","permalink":"/docs/basics","navigation":{"previous":{"title":"How Raftt Works?","permalink":"/docs/intro/how_raftt_works"},"next":{"title":"Spawn Your First Environment","permalink":"/docs/basics/quickstart"}}}')}}]);
---
+++ assets/js/3152febb.6178f328.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[225],{3492:function(t){t.exports=JSON.parse('{"title":"Getting Started","slug":"basics","permalink":"/docs/basics","navigation":{"previous":{"title":"How Raftt Works?","permalink":"/docs/intro/how_raftt_works"},"next":{"title":"quickstart","permalink":"/docs/basics/quickstart"}}}')}}]);
